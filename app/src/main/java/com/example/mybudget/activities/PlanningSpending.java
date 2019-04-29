e31797f295dc7817748436e706cf0492686a07ef
djotihamNABAGOU <djotiham.nabagou@etu.univ-cotedazur.fr>
2019-04-29 15:23:12 +0200

    create activities

---
+++ app/src/main/java/com/example/mybudget/activities/PlanningSpending.java
@@ -0,0 +1,15 @@
+package com.example.mybudget.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mybudget.R;
+
+public class PlanningSpending extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_planning_spending);
+    }
+}
---
+++ app/src/main/java/com/example/mybudget/activities/SpendingHistory.java
@@ -0,0 +1,15 @@
+package com.example.mybudget.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mybudget.R;
+
+public class SpendingHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spending_history);
+    }
+}
