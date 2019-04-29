9893295a6691dab41acbaaf41a3fa4968d352e11
djotihamNABAGOU <djotiham.nabagou@etu.univ-cotedazur.fr>
2019-04-29 16:16:03 +0200

    link between planning fragment and planning activity

--- app/src/main/java/com/example/mybudget/fragments/PlanningFragment.java
+++ app/src/main/java/com/example/mybudget/fragments/PlanningFragment.java
@@ -1,19 +1,23 @@
 package com.example.mybudget.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.mybudget.R;
+import com.example.mybudget.activities.PlanningSpending;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PlanningFragment extends Fragment {
 
+    ImageView planningSpending;
 
     public PlanningFragment() {
         // Required empty public constructor
@@ -25,6 +29,13 @@ public class PlanningFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         final View planningView = inflater.inflate(R.layout.fragment_planning, container, false);
+        planningSpending = (ImageView) planningView.findViewById(R.id.addPurchaseImage);
+        planningSpending.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), PlanningSpending.class));
+            }
+        });
         return planningView;
     }
 
