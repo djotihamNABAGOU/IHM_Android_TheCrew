fa4aeacbcdffbd91d9c23656b199f1f80e6bff97
Smith Djamoura <smith-etchanm.djamoura@etu.univ-cotedazur.fr>
2019-04-29 17:20:47 +0200

    Add Spending Activity FXML update

--- app/src/main/java/com/example/mybudget/activities/AddSpending.java
+++ app/src/main/java/com/example/mybudget/activities/AddSpending.java
@@ -2,8 +2,13 @@ package com.example.mybudget.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.TextureView;
+import android.widget.TextView;
 
 import com.example.mybudget.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class AddSpending extends AppCompatActivity {
 
@@ -11,5 +16,13 @@ public class AddSpending extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_spending);
+        setSpendingDate();
+
+    }
+
+    private void setSpendingDate(){
+        TextView spendindDate = findViewById(R.id.edSpendingDate);
+        SimpleDateFormat dateformat = new SimpleDateFormat("dd-MMM-yyyy");
+        spendindDate.setText(dateformat.format(Calendar.getInstance().getTime()));
     }
 }
