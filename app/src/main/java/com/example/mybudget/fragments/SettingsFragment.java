8e0efdf590e144822dda51d98ac801f7f11c99b3
Amine Legrifi <amine.legrifi@etu.unice.fr>
2019-04-29 16:32:26 +0200

    ajout du liste des paramètres

--- app/src/main/java/com/example/mybudget/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/mybudget/fragments/SettingsFragment.java
@@ -1,11 +1,15 @@
 package com.example.mybudget.fragments;
 
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import com.example.mybudget.R;
 
@@ -23,9 +27,25 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         final View settingsView = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        String[] settings = {"Compte","Revenus et préférences", "Aide", "Á propos","Partager"};
+
+        ListView listView = (ListView) settingsView.findViewById(R.id.paramLystview);
+
+
+        ListAdapter adapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,settings);
+
+
+        listView.setAdapter(adapter);
+
         return settingsView;
     }
+
+
+
+
 
 }
