404d61624d05e83c10c66bf327e4937f53994bbb
Amine Legrifi <amine.legrifi@etu.unice.fr>
2019-05-01 18:44:25 +0200

    ajout des pages Comptes, Revenus, Aide, A propos, Partager

---
+++ app/src/main/java/com/example/mybudget/activities/Account.java
@@ -0,0 +1,27 @@
+package com.example.mybudget.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.example.mybudget.R;
+
+public class Account extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+
+        /**ToolBar*/
+
+        Bundle bundle = getIntent().getExtras();
+        toolbar =(Toolbar) findViewById(R.id.toolBar);
+        toolbar.setTitle(bundle.getString("Activity"));
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+}
--- app/src/main/java/com/example/mybudget/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/mybudget/fragments/SettingsFragment.java
@@ -2,16 +2,23 @@ package com.example.mybudget.fragments;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import com.example.mybudget.R;
+import com.example.mybudget.activities.About;
+import com.example.mybudget.activities.Account;
+import com.example.mybudget.activities.Help;
+import com.example.mybudget.activities.MainActivity;
+import com.example.mybudget.activities.Revenus;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,17 +36,67 @@ public class SettingsFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
-        final View settingsView = inflater.inflate(R.layout.fragment_settings, container, false);
+        final View settingsView = inflater.inflate(R.layout.fragment_settings,
+                container,
+                false);
 
-        String[] settings = {"Compte","Revenus et préférences", "Aide", "Á propos","Partager"};
+        String[] settings = {"Compte",
+                "Revenus et préférences",
+                "Aide",
+                "Á propos",
+                "Partager"
+        };
 
-        ListView listView = (ListView) settingsView.findViewById(R.id.paramLystview);
+        final ListView listView = (ListView) settingsView.findViewById(R.id.paramLystview);
 
-
-        ListAdapter adapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,settings);
-
+        ListAdapter adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1,
+                settings);
 
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent,
+                                    View view,
+                                    int position,
+                                    long id) {
+
+
+                if (position==0){
+                    Intent intent = new Intent(getActivity(), Account.class);
+
+                    intent.putExtra("Activity", listView.getItemAtPosition(position).toString());
+
+                    startActivity(intent);
+                }
+                if (position==1){
+                    Intent intent = new Intent(getActivity(), Revenus.class);
+
+                    intent.putExtra("Activity", listView.getItemAtPosition(position).toString());
+
+                    startActivity(intent);
+                }
+                if (position==2){
+                    Intent intent = new Intent(getActivity(), Help.class);
+
+                    intent.putExtra("Activity", listView.getItemAtPosition(position).toString());
+
+                    startActivity(intent);
+                }
+                if (position==3){
+                    Intent intent = new Intent(getActivity(), About.class);
+
+                    intent.putExtra("Activity", listView.getItemAtPosition(position).toString());
+
+                    startActivity(intent);
+                }
+
+
+            }
+        }
+        );
 
         return settingsView;
     }
