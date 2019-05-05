f7fd4ac84f3aee3b215b147cdef891ed5bb42ba6
PaulK <merveille.koffi@etu.unice.fr>
2019-05-06 00:31:21 +0200

    Historique dépense à 50%

--- app/src/main/java/com/example/mybudget/activities/SpendingHistory.java
+++ app/src/main/java/com/example/mybudget/activities/SpendingHistory.java
@@ -3,11 +3,37 @@ package com.example.mybudget.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.content.SharedPreferences;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewStub;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ListView;
+import com.example.mybudget.adapters.SpendingListViewAdapter;
+import com.example.mybudget.adapters.SpendingGridViewAdapter;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
 import com.example.mybudget.R;
 
 public class SpendingHistory extends AppCompatActivity {
     private Toolbar toolbar;
+
+    //    ### Attrbuts
+    private ViewStub stubGrid;
+    private ViewStub stubList;
+    private ListView listView;
+    private GridView gridView;
+    private SpendingListViewAdapter listViewAdapter;
+    private SpendingGridViewAdapter gridViewAdapter;
+    private List<Spending> spendingList;
+    private int currentViewMode = 0;
+
+    static final int VIEW_MODE_LISTVIEW = 0;
+    static final int VIEW_MODE_GRIDVIEW = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +46,20 @@ public class SpendingHistory extends AppCompatActivity {
         toolbar.setSubtitle(R.string.sub_spendingHistory);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        stubList = (ViewStub) findViewById(R.id.stub_list);
+        stubGrid = (ViewStub) findViewById(R.id.stub_grid);
+
+        //Inflate ViewStub before get view
+
+        stubList.inflate();
+        stubGrid.inflate();
+
+        listView = (ListView) findViewById(R.id.spendinglistview);
+        gridView = (GridView) findViewById(R.id.spendinggridview);
     }
+
+
 }
+
