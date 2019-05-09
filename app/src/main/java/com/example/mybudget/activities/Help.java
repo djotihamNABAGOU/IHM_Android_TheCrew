7ed69355a924b413568836eaf946b51de4348c0f
Amine Legrifi <amine.legrifi@etu.unice.fr>
2019-05-10 01:14:45 +0200

    Ajout de l'activité Help avec un videoplayer +
    Partage de l'appli depuis le menu settings

--- app/src/main/java/com/example/mybudget/activities/Help.java
+++ app/src/main/java/com/example/mybudget/activities/Help.java
@@ -1,14 +1,24 @@
 package com.example.mybudget.activities;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import com.example.mybudget.R;
 
 public class Help extends AppCompatActivity {
 
     private Toolbar toolbar;
+
+
+    private VideoView videoView;
+    private int position = 0;
+    private MediaController mediaController;
 
 
     @Override
@@ -27,5 +37,93 @@ public class Help extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
+        /**VideoPlayer**/
+
+        videoView = (VideoView) findViewById(R.id.videoView);
+
+        // Set the media controller buttons
+        if (mediaController == null) {
+            mediaController = new MediaController(Help.this);
+
+            // Set the videoView that acts as the anchor for the MediaController.
+            mediaController.setAnchorView(videoView);
+
+
+            // Set MediaController for VideoView
+            videoView.setMediaController(mediaController);
+        }
+
+
+        try {
+            // ID of video file.
+            int id = this.getRawResIdByName("timer");
+            videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + id));
+
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+
+        videoView.requestFocus();
+
+
+        // When the video file ready for playback.
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+
+            public void onPrepared(MediaPlayer mediaPlayer) {
+
+
+                videoView.seekTo(position);
+                if (position == 0) {
+                    videoView.start();
+                }
+
+                // When video Screen change size.
+                mediaPlayer.setOnVideoSizeChangedListener(new MediaPlayer.OnVideoSizeChangedListener() {
+                    @Override
+                    public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
+
+                        // Re-Set the videoView that acts as the anchor for the MediaController
+                        mediaController.setAnchorView(videoView);
+                    }
+                });
+            }
+        });
+
     }
+
+    // Find ID corresponding to the name of the resource (in the directory raw).
+    public int getRawResIdByName(String resName) {
+        String pkgName = this.getPackageName();
+        // Return 0 if not found.
+        int resID = this.getResources().getIdentifier(resName, "raw", pkgName);
+        Log.i("AndroidVideoView", "Res Name: " + resName + "==> Res ID = " + resID);
+        return resID;
+    }
+
+
+    // When you change direction of phone, this method will be called.
+    // It store the state of video (Current position)
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+
+        // Store current position.
+        savedInstanceState.putInt("CurrentPosition", videoView.getCurrentPosition());
+        videoView.pause();
+    }
+
+
+    // After rotating the phone. This method is called.
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        // Get saved position.
+        position = savedInstanceState.getInt("CurrentPosition");
+        videoView.seekTo(position);
+    }
+
+
 }
+
